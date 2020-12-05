223905b3f9a748a1eaf25e8839906d3b0ffa8ec1
janis <janis.davidsons@gmail.com>
2020-12-05 22:13:54 +0200

    Option to select drag color in graph

--- interface/src/project/components/RgbSettings.js
+++ interface/src/project/components/RgbSettings.js
@@ -3,6 +3,7 @@ import Highcharts from "highcharts/highstock";
 import HighchartsReact from "highcharts-react-official";
 import HC_more from "highcharts/highcharts-more";
 import { FormActions, FormButton } from '../../components';
+import { Radio, RadioGroup, FormControl, FormLabel, FormControlLabel, } from '@material-ui/core';
 import SaveIcon from '@material-ui/icons/Save';
 
 
@@ -10,6 +11,8 @@ HC_more(Highcharts);
 require("highcharts/modules/draggable-points")(Highcharts);
 
 export default ({ setDataHandler, saveDataHandler, data }) => {
+
+  const [selectedColor, setSelectedColor] = useState('red');
 
   const [options, setOptions] = useState({
     title: {
@@ -37,19 +40,19 @@ export default ({ setDataHandler, saveDataHandler, data }) => {
       {
         name: "Red",
         color: "red",
-        dragDrop: { draggableY: true , draggableX:false},
-        data: [{y:1,x:0},{y:1,x:1},{y:1,x:2},{y:1,x:3},{y:1,x:4},{y:1,x:5},{y:1,x:6},{y:1,x:7},{y:1,x:8},{y:1,x:9},{y:1,x:10},{y:1,x:11},{y:1,x:12},{y:1,x:13},{y:1,x:14},{y:1,x:15},{y:1,x:16},{y:1,x:17},{y:1,x:18},{y:1,x:19}, {y:1,x:20},{y:1,x:21},{y:1,x:22},{y:1,x:23}]
+        dragDrop: { draggableY: true, draggableX: false },
+        data: [{ y: 1, x: 0 }, { y: 1, x: 1 }, { y: 1, x: 2 }, { y: 1, x: 3 }, { y: 1, x: 4 }, { y: 1, x: 5 }, { y: 1, x: 6 }, { y: 1, x: 7 }, { y: 1, x: 8 }, { y: 1, x: 9 }, { y: 1, x: 10 }, { y: 1, x: 11 }, { y: 1, x: 12 }, { y: 1, x: 13 }, { y: 1, x: 14 }, { y: 1, x: 15 }, { y: 1, x: 16 }, { y: 1, x: 17 }, { y: 1, x: 18 }, { y: 1, x: 19 }, { y: 1, x: 20 }, { y: 1, x: 21 }, { y: 1, x: 22 }, { y: 1, x: 23 }]
       },
       {
         name: "green",
         color: "green",
-        dragDrop: { draggableY: true },
+        dragDrop: { draggableY: false },
         data: [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200, 210, 220, 230, 240]
       },
       {
         name: "blue",
         color: "blue",
-        dragDrop: { draggableY: true },
+        dragDrop: { draggableY: false },
         data: [100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100]
       }
     ]
@@ -57,6 +60,32 @@ export default ({ setDataHandler, saveDataHandler, data }) => {
 
   const handleSubmit = () => {
     console.log('rgb setting: ', options.series[0].data)
+  }
+
+  const handleRadioChange = (event) => {
+    let newValue = event.target.value;
+    setSelectedColor(newValue);
+    setOptions({ ...options }, options.series[0].dragDrop.draggableY = false);
+    setOptions({ ...options }, options.series[1].dragDrop.draggableY = false);
+    setOptions({ ...options }, options.series[2].dragDrop.draggableY = false);
+
+    console.log({ ...options })
+    switch (newValue) {
+      case "red":
+        const redDraggable = options.series[0].dragDrop.draggableY;
+        setOptions({ ...options }, options.series[0].dragDrop.draggableY = redDraggable ? false : true)
+        break;
+      case "green":
+        const greenDraggable = options.series[1].dragDrop.draggableY;
+        setOptions({ ...options }, options.series[1].dragDrop.draggableY = greenDraggable ? false : true)
+        break;
+      case "blue":
+        const blueDraggable = options.series[2].dragDrop.draggableY;
+        setOptions({ ...options }, options.series[2].dragDrop.draggableY = blueDraggable ? false : true)
+        break;
+      default:
+        break;
+    }
   }
 
   return (
@@ -67,6 +96,16 @@ export default ({ setDataHandler, saveDataHandler, data }) => {
         highcharts={Highcharts}
         options={options}
       />
+
+      <FormControl component="fieldset" >
+        <FormLabel component="legend">Select to enagble draggable line chart</FormLabel>
+        <RadioGroup aria-label="rgb" name="rgb" value={selectedColor} onChange={handleRadioChange}>
+          <FormControlLabel value="red" control={<Radio />} label="Red" />
+          <FormControlLabel value="green" control={<Radio />} label="Green" />
+          <FormControlLabel value="blue" control={<Radio />} label="Blue" />
+        </RadioGroup>
+      </FormControl>
+
       <FormActions>
         <FormButton startIcon={<SaveIcon />} variant="contained" color="primary" type="submit" onClick={handleSubmit}>
           Save
