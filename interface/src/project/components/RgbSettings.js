5904a89f19a732806b6e037bbe541d436cf7e400
janis <janis.davidsons@gmail.com>
2020-12-06 15:48:52 +0200

    wip

--- interface/src/project/components/RgbSettings.js
+++ interface/src/project/components/RgbSettings.js
@@ -21,25 +21,34 @@ export default ({ setDataHandler, saveDataHandler, data }) => {
       zoomType: 'x',
       panning: true,
       panKey: 'shift',
-
       events: {
         load: function () {
-          this.xAxis[0].setExtremes(Date.UTC(2010, 0, 1, 9, 0, 0), Date.UTC(2010, 0, 1, 16, 0, 0));
+          this.xAxis[0].setExtremes(Date.UTC(2020, 0, 1, 9, 0, 0), Date.UTC(2020, 0, 1, 22, 0, 0));
         }
       }
     },
 
+    tooltip: {
+      followTouchMove: false
+    },
+    credits: {
+      text: '© Janis Davidsons',
+      href: ''
+    },
     yAxis: {
+      dragMaxY: 255,
 
+      min: 0,
+      max: 255
     },
     xAxis: {
-      min: Date.UTC(2010, 0, 1, 0, 0, 0),
-      max: Date.UTC(2010, 0, 2, 0, 0, 0),
+      min: Date.UTC(2020, 0, 1, 0, 0, 0),
+      max: Date.UTC(2020, 0, 2, 0, 0, 0),
       type: 'datetime',
       tickPositioner: function () {
         var info = this.tickPositions.info;
         var positions = [];
-        for (let i = Date.UTC(2010, 0, 1, 0, 0, 0); i <= Date.UTC(2010, 0, 2, 0, 0, 0); i += 3600 * 1000) {
+        for (let i = Date.UTC(2020, 0, 1, 0, 0, 0); i <= Date.UTC(2020, 0, 2, 0, 0, 0); i += 3600 * 500) {
           positions.push(i);
         }
         positions.info = info;
@@ -51,8 +60,14 @@ export default ({ setDataHandler, saveDataHandler, data }) => {
       },
       title: {
         enabled: false
+      },
+      labels: {
+        rotation: -45,
+        style: {
+          fontSize: '13px',
+          fontFamily: 'Verdana, sans-serif'
+        }
       }
-      /* categories: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'] */
     },
 
 
@@ -60,42 +75,23 @@ export default ({ setDataHandler, saveDataHandler, data }) => {
       {
         name: "Red",
         color: "red",
-        dragDrop: { draggableY: true },
+        dragDrop: {
+          draggableY: true,
+          dragMaxY: 255,
+          dragMinY:0
+        },
         data: [
           {
             y: 0,
-            x: Date.UTC(2010, 0, 1, 9, 0, 0),
-            color: 'red'
+            x: Date.UTC(2020, 0, 1, 9, 0, 0),
           },
           {
             y: 1,
-            x: Date.UTC(2010, 0, 1, 10, 0, 0),
-            color: 'red'
+            x: Date.UTC(2020, 0, 1, 10, 0, 0),
           },
           {
             y: 3,
-            x: Date.UTC(2010, 0, 1, 11, 0, 0),
-            color: 'red'
-          },
-
-        ]
-      },
-      {
-        name: "Blue",
-        color: "blue",
-        dragDrop: { draggableY: false },
-        data: [
-          {
-            y: 46,
-            x: Date.UTC(2010, 0, 1, 9, 0, 0),
-          },
-          {
-            y: 23,
-            x: Date.UTC(2010, 0, 1, 10, 0, 0),
-          },
-          {
-            y: 3,
-            x: Date.UTC(2010, 0, 1, 11, 0, 0),
+            x: Date.UTC(2020, 0, 1, 11, 0, 0),
           },
 
         ]
@@ -103,156 +99,53 @@ export default ({ setDataHandler, saveDataHandler, data }) => {
       {
         name: "Green",
         color: "green",
-        dragDrop: { draggableY: false },
+        dragDrop: {
+          draggableY: false,
+          dragMaxY: 255,
+          dragMinY:0
+        },
         data: [
           {
             y: 5,
-            x: Date.UTC(2010, 0, 1, 9, 0, 0),
+            x: Date.UTC(2020, 0, 1, 9, 0, 0),
           },
           {
             y: 156,
-            x: Date.UTC(2010, 0, 1, 10, 0, 0),
+            x: Date.UTC(2020, 0, 1, 10, 0, 0),
           },
           {
             y: 33,
-            x: Date.UTC(2010, 0, 1, 11, 0, 0),
+            x: Date.UTC(2020, 0, 1, 11, 0, 0),
           },
 
         ]
-      }
+      },
+      {
+        name: "Blue",
+        color: "blue",
+        dragDrop: {
+          draggableY: false,
+          dragMaxY: 255,
+          dragMinY:0
+        },
+        data: [
+          {
+            y: 46,
+            x: Date.UTC(2020, 0, 1, 9, 0, 0),
+          },
+          {
+            y: 23,
+            x: Date.UTC(2020, 0, 1, 10, 0, 0),
+          },
+          {
+            y: 3,
+            x: Date.UTC(2020, 0, 1, 11, 0, 0),
+          },
+
+        ]
+      },
     ]
-
-
-    //   chart: {
-    //     zoomType: 'x'
-    // },
-    // xAxis: {
-    //     type: 'datetime',
-    //     tickInterval: 3600 * 1000,
-    //     min: Date.UTC(2013,4,22),
-    //     max: Date.UTC(2013,4,23),
-    // },
-    // series: [{
-    //     data: [
-    //         [1369206795000, 1],
-    //         [1369225421000, 3],
-    //         [1369230934000, 2]
-    //     ],
-    //     pointStart: Date.UTC(2012, 5, 22),
-    //     pointInterval: 24 * 3600 * 1000 // one day
-    // }]
-
-    // rangeSelector: {
-    //   selected: 4
-    // },
-    // title: {
-    //   text: "RGB day/night cycle"
-    // },
-    // chart: {
-    //   type: "spline",
-    //   zoomType: 'x',
-    //   panning: true,
-    //   panKey: "ctrl",
-    //   pinchType: 'x',
-    //   events: {
-    //     load() {
-    //       this.xAxis[0].setExtremes(6, 16);
-    //     }
-    //   }
-    // },
-    // tooltip: {
-    //   followTouchMove: false
-    // },
-    // credits: {
-    //   text: '© Janis Davidsons',
-    //   href: ''
-    // },
-    // yAxis: {
-    //   // softMin: 0,
-    //   // softMax: 255,
-    //   categories: [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    // },
-    // xAxis: {
-    //   min: Date.UTC(2020, 0, 1, 0, 0, 0),
-    //   max: Date.UTC(2020, 0, 2, 0, 0, 0),
-    //   type: 'datetime',
-    //   tickPositioner: function () {
-    //     var info = this.tickPositions.info;
-    //     var positions = [];
-    //     for (let i = Date.UTC(2020, 0, 1, 0, 0, 0); i <= Date.UTC(2020, 0, 2, 0, 0, 0); i += 3600 * 1000) {
-    //       positions.push(i);
-    //     }
-    //     positions.info = info;
-    //     console.log(positions)
-    //     return positions;
-    //   },
-    //   lineWidth: 1,
-    //   dateTimeLabelFormats: {
-    //     day: '%H:%M'
-    //   },
-    //   // title: {
-    //   //   enabled: false
-    //   // }
-    //   // type: 'datetime',
-    //   // tickInterval: 3600 * 1000,
-    //   // min: Date.UTC(2013,4,22),
-    //   // max: Date.UTC(2013,4,23),
-    //   // // tickInterval: 2,
-    //   // // categories: [1537639969000, 1537640271000, 1537640573000, 1537640874000, 1537641176000, 1537641478000, 1537641780000, 1537642081000, 1537642383000, 1537642685000, 1537642987000, 1537643289000, 1537643590000, 1537643892000, 1537644194000, 1537644496000, 1537644797000, 1537645099000, 1537645401000, 1537645703000, 1537646004000, 1537646306000, 1537646608000, 1537646910000, 1537647212000],
-    //   // // categories: [
-    //   // //   '01:00', '02:00', '03:00', '04:00', '05:00', '06:00', '07:00', '08:00', '09:00', '10:00', '11:00', '12:00',
-    //   // //   '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00', '21:00', '22:00', '23:00', '24:00'
-    //   // // ],
-    //   // // startOnTick: false,
-    //   // tickmarkPlacement: 'on',
-    //   // title: {
-    //   //   enabled: false
-    //   // }
-    // },
-    // series: [
-    //   {
-    //     name: "Red",
-    //     color: "red",
-    //     dragDrop: { draggableY: true, draggableX: false },
-    //     showInNavigator: true,
-    //     data: [
-    //       { y: 1, x: Date.UTC(2020, 0, 1, 0, 0, 0) },
-    //       { y: 1, x: Date.UTC(2020, 0, 1, 1, 0, 0) },
-    //       { y: 1, x: Date.UTC(2020, 0, 1, 2, 0, 0) },
-    //       { y: 1, x: Date.UTC(2020, 0, 1, 3, 0, 0) },
-    //       { y: 1, x: Date.UTC(2020, 0, 1, 4, 0, 0) },
-    //       { y: 1, x: Date.UTC(2020, 0, 1, 5, 0, 0) },
-    //       { y: 1, x: Date.UTC(2020, 0, 1, 6, 0, 0) },
-    //       { y: 1, x: Date.UTC(2020, 0, 1, 7, 0, 0) },
-    //       { y: 1, x: Date.UTC(2020, 0, 1, 8, 0, 0) },
-    //       { y: 1, x: Date.UTC(2020, 0, 1, 9, 0, 0) },
-    //       // { y: 1, x: 10 }, { y: 1, x: 11 }, { y: 1, x: 12 }, { y: 1, x: 13 }, { y: 1, x: 14 }, { y: 1, x: 15 }, { y: 1, x: 16 }, { y: 1, x: 17 }, { y: 1, x: 18 }, { y: 1, x: 19 }, { y: 1, x: 20 }, { y: 1, x: 21 }, { y: 1, x: 22 }, { y: 1, x: 23 }
-    //     ],
-    //     // pointStart: Date.UTC(2012, 05, 22),
-    //     // pointInterval: 24 * 3600 * 1000 // one day
-    //   },
-    // {
-    //   name: "green",
-    //   color: "green",
-    //   dragDrop: { draggableY: false },
-    //   showInNavigator: true,
-    //   data: [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200, 210, 220, 230, 240],
-    //   // pointStart: Date.UTC(2012, 05, 22),
-    //   // pointInterval: 24 * 3600 * 1000 // one day
-    // },
-    // {
-    //   name: "blue",
-    //   color: "blue",
-    //   dragDrop: { draggableY: false },
-    //   showInNavigator: true,
-    //   data: [100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100],
-    //   // pointStart: Date.UTC(2012, 05, 22),
-    //   // pointInterval: 24 * 3600 * 1000 // one day
-    // }
-    // ]
   })
-
-  var time = [1537639969000, 1537640271000, 1537640573000, 1537640874000, 1537641176000, 1537641478000, 1537641780000, 1537642081000, 1537642383000, 1537642685000, 1537642987000, 1537643289000, 1537643590000, 1537643892000, 1537644194000, 1537644496000, 1537644797000, 1537645099000, 1537645401000, 1537645703000, 1537646004000, 1537646306000, 1537646608000, 1537646910000, 1537647212000];
 
   const handleSubmit = () => {
     console.log('rgb setting: ', options.series[0].data)
@@ -265,19 +158,24 @@ export default ({ setDataHandler, saveDataHandler, data }) => {
     setOptions({ ...options }, options.series[1].dragDrop.draggableY = false);
     setOptions({ ...options }, options.series[2].dragDrop.draggableY = false);
 
-    console.log({ ...options })
     switch (newValue) {
       case "red":
         const redDraggable = options.series[0].dragDrop.draggableY;
         setOptions({ ...options }, options.series[0].dragDrop.draggableY = redDraggable ? false : true)
+        console.log('red: ', { ...options.series })
+
         break;
       case "green":
         const greenDraggable = options.series[1].dragDrop.draggableY;
         setOptions({ ...options }, options.series[1].dragDrop.draggableY = greenDraggable ? false : true)
+        console.log('green: ', { ...options.series })
+
         break;
       case "blue":
         const blueDraggable = options.series[2].dragDrop.draggableY;
         setOptions({ ...options }, options.series[2].dragDrop.draggableY = blueDraggable ? false : true)
+        console.log('blue: ', { ...options.series })
+
         break;
       default:
         break;
@@ -305,7 +203,7 @@ export default ({ setDataHandler, saveDataHandler, data }) => {
       <FormActions>
         <FormButton startIcon={<SaveIcon />} variant="contained" color="primary" type="submit" onClick={handleSubmit}>
           Save
-            </FormButton>
+          </FormButton>
       </FormActions>
     </>
   );
